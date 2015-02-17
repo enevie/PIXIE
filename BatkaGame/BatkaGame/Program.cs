db501de07bf0bfdc90012c768daa3045671b0af5
Joseph Enev <yosif.enev@gmail.com>
2015-02-17 23:20:52 +0200

    The Game

---
+++ BatkaGame/BatkaGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatkaGame
+{
+    class Batka
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(100, 45);
+            Console.SetWindowSize(100, 45);
+            Console.WriteLine("Test");
+
+            while (true)
+            {
+
+                //the game!
+                Console.Clear();
+                
+            }
+        }
+    }
+}
