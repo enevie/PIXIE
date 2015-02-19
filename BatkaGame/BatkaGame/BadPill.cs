2490dec43968e535c8cc402e0310c3ea3b5f52c2
plamenti <plamenti_@abv.bg>
2015-02-20 00:17:08 +0200

    BadPill class

---
+++ BatkaGame/BatkaGame/BadPill.cs
@@ -0,0 +1,13 @@
+﻿namespace BatkaGame
+{
+    class BadPill : Pill
+    {
+        public BadPill(int xCoord, int yCoord, char symbol = '$')
+        {
+            this.XCoord = xCoord;
+            this.YCoord = yCoord;
+            this.Symbol = symbol;
+            this.Draw();   
+        }
+    }
+}
---
+++ BatkaGame/BatkaGame/Pill.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatkaGame
+{
+    abstract class Pill : IDraw
+    {
+        int xCoord;
+        int yCoord;
+        char symbol;
+
+
+        public int XCoord
+        {
+            get
+            {
+                return this.xCoord;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("xCoord must be >= 0");
+                }
+                else
+                {
+                    this.xCoord = value;
+                }
+
+            }
+        }
+        public int YCoord
+        {
+            get
+            {
+                return this.yCoord;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("yCoord must be >= 0");
+                }
+                else
+                {
+                    this.yCoord = value;
+                }
+
+            }
+        }
+        public char Symbol
+        {
+            get
+            {
+                return this.symbol;
+            }
+            set
+            {
+                this.symbol = value;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(this.YCoord, this.XCoord);
+            Console.Write(this.Symbol);
+        }
+    }
+}
