4ced8b7d174c664e29af2855c04d153a64004e1c
plamenti <plamenti_@abv.bg>
2015-02-20 03:33:38 +0200

    Direction

---
+++ BatkaGame/BatkaGame/Direction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatkaGame
+{
+    struct Direction
+    {
+        private int row;
+        private int col;
+
+        public Direction(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+        public int Row
+        {
+            get
+            {
+                return row;
+            }
+            set
+            {
+                this.row = value;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                return this.col;
+            }
+            set
+            {
+                this.col = value;
+            }
+        }
+    }
+}
