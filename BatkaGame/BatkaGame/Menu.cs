55cfa3edb5ab1ce7f961e125b88e1e2b7f3b05d1
Slavi Traikov <slavi.asia@gmail.com>
2015-02-20 20:47:47 +0200

    Game Menu

---
+++ BatkaGame/BatkaGame/Menu.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatkaGame
+{
+    class Menu
+    {
+        public static int ChooseListBoxItem(string[] items, int ucol, int urow, ConsoleColor back, ConsoleColor fore)
+        {
+            int numItems = items.Length;
+            int maxLength = items[0].Length;
+            for (int i = 1; i < numItems; i++)
+            {
+                if (items[i].Length > maxLength)
+                {
+                    maxLength = items[i].Length;
+                }
+            }
+            int[] rightSpaces = new int[numItems];
+            for (int i = 0; i < numItems; i++)
+            {
+                rightSpaces[i] = maxLength - items[i].Length + 1;
+            }
+            int lcol = ucol + maxLength + 3;
+            int lrow = urow + numItems + 1;
+            DrawBox(ucol, urow, lcol, lrow, back, fore, true);
+            WriteColorString(" " + items[0] + new string(' ', rightSpaces[0]), ucol + 1, urow + 1, fore, back);
+            for (int i = 2; i <= numItems; i++)
+            {
+                WriteColorString(items[i - 1], ucol + 2, urow + i, back, fore);
+            }
+            ConsoleKeyInfo cki;
+            char key;
+            int choice = 1;
+
+            while (true)
+            {
+                cki = Console.ReadKey(true);
+                key = cki.KeyChar;
+                if (key == '\r') // enter 
+                {
+                    return choice;
+                }
+                else if (cki.Key == ConsoleKey.DownArrow)
+                {
+                    WriteColorString(" " + items[choice - 1] + new string(' ', rightSpaces[choice - 1]), ucol + 1, urow + choice, back, fore);
+                    if (choice < numItems)
+                    {
+                        choice++;
+                    }
+                    else
+                    {
+                        choice = 1;
+                    }
+                    WriteColorString(" " + items[choice - 1] + new string(' ', rightSpaces[choice - 1]), ucol + 1, urow + choice, fore, back);
+                }
+                else if (cki.Key == ConsoleKey.UpArrow)
+                {
+                    WriteColorString(" " + items[choice - 1] + new string(' ', rightSpaces[choice - 1]), ucol + 1, urow + choice, back, fore);
+                    if (choice > 1)
+                    {
+                        choice--;
+                    }
+                    else
+                    {
+                        choice = numItems;
+                    }
+                    WriteColorString(" " + items[choice - 1] + new string(' ', rightSpaces[choice - 1]), ucol + 1, urow + choice, fore, back);
+                }
+            }
+        }
+        public static void DrawBox(int ucol, int urow, int lcol, int lrow, ConsoleColor back, ConsoleColor fore, bool fill)
+        {
+            const char Horizontal = '\u2500';
+            const char Vertical = '\u2502';
+            const char UpperLeftCorner = '\u250c';
+            const char UpperRightCorner = '\u2510';
+            const char LowerLeftCorner = '\u2514';
+            const char LowerRightCorner = '\u2518';
+            string fillLine = fill ? new string(' ', lcol - ucol - 1) : "";
+            SetColors(back, fore);
+            // draw top edge 
+            Console.SetCursorPosition(ucol, urow);
+            Console.Write(UpperLeftCorner);
+            for (int i = ucol + 1; i < lcol; i++)
+            {
+                Console.Write(Horizontal);
+            }
+            Console.Write(UpperRightCorner);
+            for (int i = urow + 1; i < lrow; i++)
+            {
+                Console.SetCursorPosition(ucol, i);
+                Console.Write(Vertical);
+                if (fill) Console.Write(fillLine);
+                Console.SetCursorPosition(lcol, i);
+                Console.Write(Vertical);
+            }
+            Console.SetCursorPosition(ucol, lrow);
+            Console.Write(LowerLeftCorner);
+            for (int i = ucol + 1; i < lcol; i++)
+            {
+                Console.Write(Horizontal);
+            }
+            Console.Write(LowerRightCorner);
+        }
+        public static void WriteColorString(string s, int col, int row, ConsoleColor back, ConsoleColor fore)
+        {
+            SetColors(back, fore);
+            Console.SetCursorPosition(col, row);
+            Console.Write(s);
+        }
+        public static void SetColors(ConsoleColor back, ConsoleColor fore)
+        {
+            Console.BackgroundColor = back;
+            Console.ForegroundColor = fore;
+        }
+        public static void CleanUp()
+        {
+            Console.ResetColor();
+            Console.CursorVisible = true;
+            Console.Clear();
+        }
+    }
+}
